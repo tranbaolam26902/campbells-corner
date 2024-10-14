926380c7b466f70f61fbe05b9730b930d8e6beb5
campbells <tranbaolam26902@gmail.com>
2024-10-14 16:27:36 +0700

    increase nav-item font weight

--- src/components/shared/Header/NavItem.tsx
+++ src/components/shared/Header/NavItem.tsx
@@ -16,7 +16,7 @@ export function NavItem({ href, name }: NavItemProps) {
     return (
         <Link
             href={href}
-            className={`px-2 font-medium underline-offset-4 transition-all duration-700 hover:underline ${pathname === href ? 'underline' : ''}`}
+            className={`px-2 font-semibold underline-offset-4 transition-all duration-700 hover:underline ${pathname === href ? 'underline' : ''}`}
         >
             {name}
         </Link>
@@ -29,7 +29,7 @@ export function MobileNavItem({ href, name, onClick }: NavItemProps) {
     return (
         <Link
             href={href}
-            className={`px-4 py-2 text-left font-medium underline-offset-4 hover:bg-gray-100 ${pathname === href ? 'underline' : ''}`}
+            className={`px-4 py-2 text-left font-semibold underline-offset-4 hover:bg-gray-100 ${pathname === href ? 'underline' : ''}`}
             onClick={onClick}
         >
             {name}
