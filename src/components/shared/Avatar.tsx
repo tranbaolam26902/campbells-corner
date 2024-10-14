772e2905f3fab20917c96ea72cdc5bae7cd6a181
campbells <tranbaolam26902@gmail.com>
2024-10-14 20:11:56 +0700

    enable priority for avatar

--- src/components/shared/Avatar.tsx
+++ src/components/shared/Avatar.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function Avatar() {
     return (
         <div className='mx-auto w-fit rounded-full border-8 border-white bg-secondary-200 px-4 py-3 sm:mx-0'>
-            <Image src={avatar} alt='avatar' />
+            <Image src={avatar} alt='avatar' priority />
         </div>
     );
 }
