2852fa1fa1e6a8f39fa4c32f1789a66cc820a563
campbells <tranbaolam26902@gmail.com>
2024-10-14 21:42:07 +0700

    update props type

--- src/components/shared/Container.tsx
+++ src/components/shared/Container.tsx
@@ -1,11 +1,7 @@
 // packages
-import { ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 
-interface ContainerProps {
-    children: ReactNode;
-}
-
-export default function Container({ children }: ContainerProps) {
+export default function Container({ children }: PropsWithChildren) {
     return (
         <main
             className={`mx-auto my-4 mt-0 flex flex-col gap-y-8 p-4 sm:mt-4 sm:max-w-lg md:px-0`}
