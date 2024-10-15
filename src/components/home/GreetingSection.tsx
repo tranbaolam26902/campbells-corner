b812b8aea2906729c3a58313d39219d551a71ceb
campbells <tranbaolam26902@gmail.com>
2024-10-15 14:13:06 +0700

    fix full-width message box on mobile devices

--- src/components/home/GreetingSection.tsx
+++ src/components/home/GreetingSection.tsx
@@ -5,11 +5,13 @@ export default function GreetingSection() {
     return (
         <section className='items-center gap-x-4 sm:flex'>
             <Avatar />
-            <div>
+            <div className='mx-auto flex w-fit flex-col sm:mx-0'>
                 <h1 className='mb-4 mt-4 text-center text-2xl font-semibold sm:mt-0 sm:text-left'>
                     Tran &#34;Campbell&#34; Bao Lam
                 </h1>
-                <MessageDialog message='Hi, welcome to my space on the internet!' />
+                <MessageDialog
+                    messages={['Hi, welcome to my space on the internet!']}
+                />
             </div>
         </section>
     );
