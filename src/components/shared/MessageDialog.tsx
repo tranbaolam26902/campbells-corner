b5d3a6f67c0a208e461b692ffc01910211f45cf5
campbells <tranbaolam26902@gmail.com>
2024-10-15 14:01:32 +0700

    fix accessibility issue

--- src/components/shared/MessageDialog.tsx
+++ src/components/shared/MessageDialog.tsx
@@ -5,9 +5,9 @@ interface MessageDialogProps {
 export default function MessageDialog({ message }: MessageDialogProps) {
     return (
         <div className='mx-auto flex w-fit animate-typewriter justify-center rounded-lg border border-ebony bg-white px-4 py-2 font-medium sm:mx-0'>
-            <h4 className='text-nowrap animate-cursor overflow-hidden text-center'>
+            <span className='text-nowrap animate-cursor overflow-hidden text-center'>
                 {message}
-            </h4>
+            </span>
         </div>
     );
 }
