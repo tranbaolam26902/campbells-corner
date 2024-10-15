2b46bce2443c915a199459fc45ad2a5bcd249170
campbells <tranbaolam26902@gmail.com>
2024-10-15 14:17:17 +0700

    update posts and uses page contents

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,28 @@
+// packages
 import type { Metadata } from 'next';
+
+// components
+import { Avatar, MessageDialog } from '@/components/shared';
 
 export const metadata: Metadata = {
     title: 'Posts'
 };
 
 export default function Page() {
-    return <h1>Posts page</h1>;
+    return (
+        <>
+            <section className='mx-auto flex w-fit flex-col items-center gap-y-4'>
+                <Avatar />
+                <MessageDialog
+                    messages={[
+                        'Looks like I haven’t posted anything yet.',
+                        'But I can’t wait to share my thoughts with you soon! Stay tuned!'
+                    ]}
+                />
+            </section>
+            <section>
+                <h1 className='text-center font-semibold'>Posts</h1>
+            </section>
+        </>
+    );
 }
--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -1,9 +1,28 @@
+// packages
 import type { Metadata } from 'next';
+
+// components
+import { Avatar, MessageDialog } from '@/components/shared';
 
 export const metadata: Metadata = {
     title: 'Uses'
 };
 
 export default function Page() {
-    return <h1>Uses page</h1>;
+    return (
+        <>
+            <section className='mx-auto flex w-fit flex-col items-center gap-y-4'>
+                <Avatar />
+                <MessageDialog
+                    messages={[
+                        'Curious about the tech gear I use?',
+                        'Check back soon for detailed reviews and insights!'
+                    ]}
+                />
+            </section>
+            <section>
+                <h1 className='text-center font-semibold'>Uses</h1>
+            </section>
+        </>
+    );
 }
