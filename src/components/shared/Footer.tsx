6c17ce8a6de9bcb19ced7d7d2c9384fa97d41139
campbells <tranbaolam26902@gmail.com>
2024-10-15 18:07:41 +0700

    increase default font size to 1.125rem - 18px

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer role='contentinfo' className='mb-8 text-center text-sm'>
+        <footer role='contentinfo' className='mb-8 text-center text-base'>
             &copy; {new Date().getFullYear()} Tran Bao Lam. All Rights Reserved.
         </footer>
     );
