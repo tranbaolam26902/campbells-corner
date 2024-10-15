37c319a7e6ec54b08b3fd03cb8f954c82bc24304
campbells <tranbaolam26902@gmail.com>
2024-10-15 14:12:33 +0700

    allows message box render multiple lines

--- src/components/shared/MessageDialog.tsx
+++ src/components/shared/MessageDialog.tsx
@@ -1,13 +1,18 @@
 interface MessageDialogProps {
-    message: string;
+    messages: Array<string>;
 }
 
-export default function MessageDialog({ message }: MessageDialogProps) {
+export default function MessageDialog({ messages }: MessageDialogProps) {
     return (
-        <div className='mx-auto flex w-fit animate-typewriter justify-center rounded-lg border border-ebony bg-white px-4 py-2 font-medium sm:mx-0'>
-            <span className='text-nowrap animate-cursor overflow-hidden text-center'>
-                {message}
-            </span>
+        <div className='mx-auto flex w-fit animate-typewriter flex-col justify-center rounded-lg border border-ebony bg-white px-4 py-2 font-medium sm:mx-0'>
+            {messages.map((message) => (
+                <span
+                    key={message}
+                    className='text-nowrap animate-cursor overflow-hidden text-center'
+                >
+                    {message}
+                </span>
+            ))}
         </div>
     );
 }
