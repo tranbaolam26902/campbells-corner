456d8bbc770d20d82364ca071bab540f5ae16fd7
campbells <tranbaolam26902@gmail.com>
2024-10-19 00:30:36 +0700

    change page transition animation

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -10,6 +10,7 @@ export default function Template({ children }: PropsWithChildren) {
             <motion.main
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, ease: 'easeInOut' }}
                 className={`mx-auto my-4 mt-0 flex flex-col gap-y-8 p-4 sm:mt-4 sm:max-w-lg md:px-0`}
             >
                 {children}
