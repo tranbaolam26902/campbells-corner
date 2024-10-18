aadaef8e2bdc8a3d00c956b615e635a3a60ff137
campbells <tranbaolam26902@gmail.com>
2024-10-18 18:48:36 +0700

    temporary handling not found mdx file

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -14,5 +14,7 @@ export default function Page({ params }: { params: { slug: string } }) {
 
     const Content = projects[project.slug];
 
+    if (!Content) notFound();
+
     return <Content />;
 }
