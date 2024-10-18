2f181d63500f0f6b19456679d1587477c9fd664a
campbells <tranbaolam26902@gmail.com>
2024-10-18 19:11:28 +0700

    add posts and uses mdx container

--- content/index.tsx
+++ content/index.tsx
@@ -3,6 +3,10 @@ import { MDXContentDictionary } from '@/libs/definitions';
 
 import RegionalEducationSystem from './projects/regional-education-system.mdx';
 
+export const posts: MDXContentDictionary = {};
+
 export const projects: MDXContentDictionary = {
     'regional-education-system': RegionalEducationSystem
 };
+
+export const uses: MDXContentDictionary = {};
