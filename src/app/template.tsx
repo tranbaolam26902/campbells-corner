7409713c33ebd5a03de7d2c6383f22c6abd5e095
campbells <tranbaolam26902@gmail.com>
2024-10-18 18:46:03 +0700

    change page transition effect

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -8,7 +8,7 @@ export default function Template({ children }: PropsWithChildren) {
     return (
         <AnimatePresence>
             <motion.main
-                initial={{ y: 16, opacity: 0 }}
+                initial={{ y: -32, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 className={`mx-auto my-4 mt-0 flex flex-col gap-y-8 p-4 sm:mt-4 sm:max-w-lg md:px-0`}
             >
