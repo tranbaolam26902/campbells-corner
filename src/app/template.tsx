743c62781c86079645bf9ff6999f5cf4368454fe
campbells <tranbaolam26902@gmail.com>
2024-10-19 00:07:48 +0700

    change page transition effect, testing layout shift

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -8,8 +8,8 @@ export default function Template({ children }: PropsWithChildren) {
     return (
         <AnimatePresence>
             <motion.main
-                initial={{ y: 16, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
                 className={`mx-auto my-4 mt-0 flex flex-col gap-y-8 p-4 sm:mt-4 sm:max-w-lg md:px-0`}
             >
                 {children}
