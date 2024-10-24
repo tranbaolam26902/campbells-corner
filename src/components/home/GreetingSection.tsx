1de4cd491c5e731ba33a8191f1305b103541fccb
campbells <tranbaolam26902@gmail.com>
2024-10-24 15:36:16 +0700

    center greeting section

--- src/components/home/GreetingSection.tsx
+++ src/components/home/GreetingSection.tsx
@@ -3,7 +3,7 @@ import { Avatar, MessageDialog } from '@/components/shared';
 
 export default function GreetingSection() {
     return (
-        <section className='items-center gap-x-4 sm:flex md:ml-8 lg:ml-20'>
+        <section className='gap-x-4 sm:flex sm:items-center md:mx-auto md:-translate-x-14'>
             <Avatar />
             <div className='flex flex-col'>
                 <h1 className='mb-4 mt-4 text-center text-3xl font-semibold sm:mt-0 sm:text-left'>
