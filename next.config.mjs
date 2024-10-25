1799591f4e86c821d7ecffb347c9fee0b5b32c5a
campbells <tranbaolam26902@gmail.com>
2024-10-25 21:19:54 +0700

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+    experimental: {
+        turbotrace: {}
+    }
+};
 
 export default nextConfig;
