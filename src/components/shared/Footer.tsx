972f19cbee507de5cc11626306219e23cb7d5a4f
campbells <tranbaolam26902@gmail.com>
2024-10-26 23:32:53 +0700

    update footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
 export default function Footer() {
     return (
         <footer className='mb-8 text-center text-base'>
-            &copy; {new Date().getFullYear()} Tran Bao Lam. All Rights Reserved.
+            <p>
+                &copy; {new Date().getFullYear()} Tran Bao Lam. All Rights
+                Reserved.
+            </p>
+            <p>
+                <span>Inspired by&nbsp;</span>
+                <Link href='https://craftz.dog' target='_blank'>
+                    Takuya Matsuyama&apos;s website.
+                </Link>
+            </p>
         </footer>
     );
 }
