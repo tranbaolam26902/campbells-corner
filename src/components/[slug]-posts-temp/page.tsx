7fa1883935117b3d0b3a3a186d958d48a763975f
campbells <tranbaolam26902@gmail.com>
2024-10-30 19:46:10 +0700

    temporary comment for deploy

--- src/components/[slug]-posts-temp/page.tsx
+++ src/components/[slug]-posts-temp/page.tsx
@@ -1,52 +1,52 @@
-// packages
-import { notFound } from 'next/navigation';
-import type { Metadata } from 'next';
-
-// components
-import { Back, Markdown } from '@/components/index';
-
-// utils
-import { readFileContent } from '@/utils/content-file-utils';
-import { findPostBySlug, getPostsByPath } from '@/libs/actions';
-
-interface Props {
-    params: {
-        slug: string;
-    };
-}
-
-export function generateMetadata({ params }: Props): Metadata {
-    const posts = getPostsByPath('/posts');
-    const post = findPostBySlug(posts, params.slug);
-
-    if (post === null) notFound();
-
-    return {
-        title: post.name,
-        openGraph: {
-            images: post.previewImg
-        }
-    };
-}
-
-export function generateStaticParams() {
-    const posts = getPostsByPath('/posts');
-
-    return posts.map((project) => project.slug);
-}
-
-export default function Page({ params }: Props) {
-    const posts = getPostsByPath('/posts');
-    const post = findPostBySlug(posts, params.slug);
-
-    if (post === null) notFound();
-
-    const source = readFileContent(post.path);
-
-    return (
-        <>
-            <Back />
-            <Markdown source={source} />
-        </>
-    );
-}
+// // packages
+// import { notFound } from 'next/navigation';
+// import type { Metadata } from 'next';
+//
+// // components
+// import { Back, Markdown } from '@/components/index';
+//
+// // utils
+// import { readFileContent } from '@/utils/content-file-utils';
+// import { findPostBySlug, getPostsByPath } from '@/libs/actions';
+//
+// interface Props {
+//     params: {
+//         slug: string;
+//     };
+// }
+//
+// export function generateMetadata({ params }: Props): Metadata {
+//     const posts = getPostsByPath('/posts');
+//     const post = findPostBySlug(posts, params.slug);
+//
+//     if (post === null) notFound();
+//
+//     return {
+//         title: post.name,
+//         openGraph: {
+//             images: post.previewImg
+//         }
+//     };
+// }
+//
+// export function generateStaticParams() {
+//     const posts = getPostsByPath('/posts');
+//
+//     return posts.map((project) => project.slug);
+// }
+//
+// export default function Page({ params }: Props) {
+//     const posts = getPostsByPath('/posts');
+//     const post = findPostBySlug(posts, params.slug);
+//
+//     if (post === null) notFound();
+//
+//     const source = readFileContent(post.path);
+//
+//     return (
+//         <>
+//             <Back />
+//             <Markdown source={source} />
+//         </>
+//     );
+// }
