11a3bf68309652d40e0d60faa1c818354fd847d1
campbells <tranbaolam26902@gmail.com>
2024-11-04 21:11:10 +0700

    fix scrolling animation

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -7,6 +7,7 @@ import { LayoutRouterContext } from 'next/dist/shared/lib/app-router-context.sha
 import { usePathname } from 'next/navigation';
 
 function FrozenRoute({ children }: PropsWithChildren) {
+    // hooks
     const context = useContext(LayoutRouterContext);
     const frozen = useRef(context).current;
 
@@ -18,15 +19,23 @@ function FrozenRoute({ children }: PropsWithChildren) {
 }
 
 export default function PageTransition({ children }: PropsWithChildren) {
+    // hooks
     const pathname = usePathname();
 
+    // event handlers
+    const handleExitComplete = () => {
+        if (typeof window !== 'undefined') {
+            window.scrollTo({ top: 0 });
+        }
+    };
+
     return (
-        <AnimatePresence mode='wait'>
+        <AnimatePresence mode='wait' onExitComplete={handleExitComplete}>
             <motion.main
                 key={pathname}
-                initial={{ opacity: 0, y: 16 }}
+                initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 16 }}
+                exit={{ opacity: 0, y: 20 }}
                 transition={{ duration: 0.4 }}
                 className='mx-auto my-4 mt-0 flex flex-col gap-y-8 p-4 sm:mt-4 sm:max-w-xl md:max-w-2xl lg:max-w-3xl'
             >
