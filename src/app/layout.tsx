e435f8d219d74128990cdfb08be9fa8d85eda1f2
campbells <tranbaolam26902@gmail.com>
2024-11-11 22:15:46 +0700

    remove analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 // packages
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
-import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 // styles
@@ -97,7 +96,6 @@ export default function RootLayout({
                     <Footer />
                     <ScrollToTop />
                 </ThemeWrapper>
-                <Analytics />
                 <SpeedInsights />
             </body>
         </html>
