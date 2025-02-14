f260feb8f4f4970ef649f562c0b5f143b9d921c9
campbells <tranbaolam26902@gmail.com>
2025-02-15 00:35:37 +0700

    fix footer year updating dynamically

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 // packages
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function Footer() {
+    // states
+    const [currentYear, setCurrentYear] = useState<number>(
+        new Date().getFullYear()
+    );
+
+    // side effects
+    useEffect(() => {
+        setCurrentYear(new Date().getFullYear());
+    }, []);
+
     return (
         <footer className='mb-8 text-center text-base'>
-            <p>
-                &copy; {new Date().getFullYear()} Tran Bao Lam. All Rights
-                Reserved.
-            </p>
+            <p>&copy; {currentYear} Tran Bao Lam. All Rights Reserved.</p>
             <p>
                 <span>Inspired by&nbsp;</span>
                 <Link href='https://craftz.dog' target='_blank'>
